4f0f7a6619ae88167a3a8f414971333718e13101
Saif Ullah <mrsaifullah52.1@gmail.com>
2021-01-07 01:59:45 +0500

    Added Dashboard(Main Screen Area)

--- app/src/main/java/com/softtechglobal/androidcarmanager/MainActivity.java
+++ app/src/main/java/com/softtechglobal/androidcarmanager/MainActivity.java
@@ -3,26 +3,37 @@ package com.softtechglobal.androidcarmanager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.card.MaterialCardView;
 import com.softtechglobal.androidcarmanager.usermanagement.Signin;
+import com.softtechglobal.androidcarmanager.usermanagement.Signup;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn;
+    MaterialCardView cardView1,cardView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn=(Button)findViewById(R.id.btn);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        cardView1=(MaterialCardView)findViewById(R.id.card1);
+        cardView2=(MaterialCardView)findViewById(R.id.card2);
+
+        cardView1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i=new Intent(MainActivity.this, Signin.class);
+                startActivity(i);
+            }
+        });
+
+        cardView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this, Signup.class);
                 startActivity(i);
             }
         });
