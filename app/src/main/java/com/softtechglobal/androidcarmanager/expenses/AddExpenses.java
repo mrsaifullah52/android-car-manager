d2cb22ab76500b6591dc2075b104902b1c4af6cd
Saif Ullah <mrsaifullah52.1@gmail.com>
2021-01-14 22:35:11 +0500

    Client Side has been Complete

--- app/src/main/java/com/softtechglobal/androidcarmanager/expenses/AddExpenses.java
+++ app/src/main/java/com/softtechglobal/androidcarmanager/expenses/AddExpenses.java
@@ -1,16 +1,40 @@
 package com.softtechglobal.androidcarmanager.expenses;
 
-import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.softtechglobal.androidcarmanager.R;
 
-public class AddExpenses extends AppCompatActivity {
+public class AddExpenses extends RecyclerView.ViewHolder {
+    public final ImageView icon;
+    public final TextView text;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_addexpenses);
+    public AddExpenses(@NonNull View itemView) {
+        super(itemView);
+
+        this.icon = itemView.findViewById(R.id.mtrl_list_item_icon);
+        this.text = itemView.findViewById(R.id.mtrl_list_item_text);
     }
+
+    @NonNull
+    public static AddExpenses create(@NonNull ViewGroup parent) {
+        return new AddExpenses(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.activity_addexpenses, parent, false));
+    }
+
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_addexpenses);
+//        this.icon = itemView.findViewById(R.id.mtrl_list_item_icon);
+//        this.text = itemView.findViewById(R.id.mtrl_list_item_text);
+//
+//    }
 }
